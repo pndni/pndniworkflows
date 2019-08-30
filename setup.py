ad1670c62bccc209d621adc632cf60122bde42b4
Steven Tilley <stilley@hollandbloorview.ca>
2019-08-30 13:10:20 -0400

    add dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ from setuptools import setup, find_packages
 setup(
     name='pndniworkflows',
     version='dev',
+    install_requires=[
+        'matplotlib>=3',
+        'nipype>=1.2',
+        'Jinja2>=2.10.1',
+        'nibabel>=2.4.0'
+        ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    extra_require={
+        'doc': ['Sphinx', 'sphinx-argparse', 'sphinx-rtd-theme']
+    },
     packages=find_packages(),
 )
