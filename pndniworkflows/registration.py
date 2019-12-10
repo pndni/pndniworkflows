08f72099b323e03c9cbf064a88270bab248196a4
Steven Tilley <steve@steventilley.com>
2019-12-10 12:31:26 -0500

    add ants_registration_syn_no_affine_node

--- pndniworkflows/registration.py
+++ pndniworkflows/registration.py
@@ -1,6 +1,39 @@
 from nipype.interfaces.ants.registration import Registration
 
 
+def ants_registration_syn_no_affine_node(**kwargs):
+    """return antsRegistration interace instance with default values
+    based on antsRegistrationSyN.sh with the s transformation option
+    and the rigid and affine steps removed
+
+    :param \\*\\*kwargs: parameters to override the default values
+    :return: :py:obj:`Registration` node
+    """
+    defaults = dict(dimension=3,
+                    use_histogram_matching=False,
+                    interpolation='Linear',
+                    metric=['CC'],
+                    metric_weight=[1.0],
+                    radius_or_number_of_bins=[4],
+                    sampling_strategy=[None],
+                    sampling_percentage=[None],
+                    transforms=['SyN'],
+                    transform_parameters=[(0.1, 3, 0)],
+                    smoothing_sigmas=[[3, 2, 1, 0]],
+                    sigma_units=['vox'],
+                    shrink_factors=[[8, 4, 2, 1]],
+                    number_of_iterations=[[100, 70, 50, 20]],
+                    convergence_threshold=[1e-6],
+                    convergence_window_size=[10],
+                    winsorize_lower_quantile=0.005,
+                    winsorize_upper_quantile=0.995,
+                    write_composite_transform=True,
+                    output_warped_image=True)
+    defaults.update(kwargs)
+    ants = Registration(**defaults)
+    return ants
+
+
 def ants_registration_syn_node(**kwargs):
     """return antsRegistration interace instance with default values
     based on antsRegistrationSyN.sh with the s transformation option
